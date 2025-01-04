646da8e9cdb88b2b87be891c32d24c48bce0bbff
kennethazin <kenneth.ras3@mail.dcu.ie>
2025-01-04 22:37:03 +0000

    finish UI

---
+++ src/components/shorten-form.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React from "react";
+import { useState } from "react";
+import { Input } from "./ui/input";
+import { Button } from "./ui/button";
+
+export default function ShortenForm() {
+const [url, setUrl] = useState<string>('');
+  
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault(); // prevents reloading the page
+    console.log(url);
+
+  }
+  return (
+    <form onSubmit={handleSubmit} className="mb-4">
+      <div className="space-y-4">
+        <Input
+          value={url}
+          onChange={(e) => setUrl(e.target.value)}
+          className="h-12"
+          type="url"
+          placeholder="Enter URL to shorten"
+          required
+        />
+        <Button className="w-full p-2" type="submit">
+          Shorten URL
+        </Button>
+      </div>
+    </form>
+  );
+}
