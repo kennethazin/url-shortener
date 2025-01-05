b70628b7a2aca3e211c46ceb1b07defad78bc10e
kennethazin <kenneth.ras3@mail.dcu.ie>
2025-01-05 17:05:20 +0000

    feat: update global styles, enhance URL list skeleton, and improve toast hook

--- src/components/url-list-skeleton.tsx
+++ src/components/url-list-skeleton.tsx
@@ -3,14 +3,12 @@ import { Skeleton } from './ui/skeleton';
 
 const UrlListSkeleton = () => (
   <div>
-    <h2 className="text-2xl font-bold mb-2">Recent URLs</h2>
     <ul className="space-y-5">
       {[...Array(5)].map((_, index) => (
         <li key={index} className="flex items-center gap-5 justify-between">
-      <Skeleton className="h-5 w-1/2 " />
+      <Skeleton className="h-20 w-full " />
       <div className="flex items-center gap-6">
-      <Skeleton className="h-5 w-5" />
-      <Skeleton className="h-5 w-20" />
+
       </div>
         </li>
       ))}
